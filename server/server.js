51f9a5a5677ef4e9733839ebca954cebe343a9ee
Amey Chintale <abchintale2003@gmail.com>
2026-02-06 00:32:26 +0530

    Final production configuration: Updated CORS and PayPal URLs

--- server/server.js
+++ server/server.js
@@ -29,15 +29,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
+    origin: [
+      "http://localhost:5173", 
+      "https://ecommerce-website-ochre-three.vercel.app" 
     ],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma"],
     credentials: true,
   })
 );
